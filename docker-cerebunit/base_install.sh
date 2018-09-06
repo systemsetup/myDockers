deb860760babdada25ec8b3b4093ee0d7c8b142b
lungsi <neuralgraphs@gmail.com>
2018-09-06 08:55:37 +0000

    updated comments

--- docker-cerebunit/base_install.sh
+++ docker-cerebunit/base_install.sh
@@ -12,6 +12,7 @@
 
 # Base installations
 apt-get install -y wget bzip2 vim tmux build-essential checkinstall libopenmpi-dev openmpi-bin openmpi-doc python2.7 python-pip python-tk python-lxml libhdf5-serial-dev
+# python-tk is for pylab and python-lxml libhdf5-serila-dev for NWB
 
 # Install for NEURON
 apt-get install -y libx11-dev libxext-dev mpich libncurses-dev
