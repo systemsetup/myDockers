461066bd1f90b981d15125ea85adbdbe8adb3fe2
lungsi <lungsi@users.noreply.github.com>
2019-03-12 09:53:26 +0100

    Update py27_install.sh

--- docker-pynnmoose/py27_install.sh
+++ docker-pynnmoose/py27_install.sh
@@ -14,28 +14,32 @@ source /envs/py27-pyNNmoose/bin/activate
 pip install -U numpy scipy matplotlib pandas mpi4py ipython jupyter quantities neo elephant pynwb sciunit bokeh
 pip install dask distributed --upgrade
 
-#cd /
-#mkdir simulators && cd simulators
+# Got to root directory and create an installation directory where the installed simulator will be located
+cd / && mkdir simulators && cd simulators
 
 ############################### NEURON ##################################
-# Install NEURON setup
-mkdir neuron7.5 && cd neuron7.5
-#wget "http://www.neuron.yale.edu/ftp/neuron/versions/v7.5/nrn-7.5.tar.gz"
-#wget "http://www.neuron.yale.edu/ftp/neuron/versions/v7.5/iv-19.tar.gz"
-#tar xzf nrn-7.5.tar.gz && tar xzf iv-19.tar.gz
-#mkdir iv && mkdir nrn
+# Make installation directory go there and make two directories for installing the extracted packages
+mkdir neuron7.5 && cd neuron7.5 && mkdir iv && mkdir nrn
+# Download installation package in the created installation root directory
+wget "http://www.neuron.yale.edu/ftp/neuron/versions/v7.5/nrn-7.5.tar.gz"
+wget "http://www.neuron.yale.edu/ftp/neuron/versions/v7.5/iv-19.tar.gz"
+# Extract them
+tar xzf nrn-7.5.tar.gz && tar xzf iv-19.tar.gz
 # Preinstall configure
-#cd iv-19 && ./configure --prefix='/simulators/neuron7.5/iv'
-#make && make install clean
-#cd .. && cd nrn-7.5
-#./configure --prefix='/simulators/neuron7.5/nrn' --with-nrnpython --with-paranrn --with-iv --disable-rx3d
-#make && make install clean
-# Install
+cd iv-19 && ./configure --prefix='/simulators/neuron7.5/iv'
+make && make install clean
+# Do the same for the other extracted package
+cd .. && cd nrn-7.5
+./configure --prefix='/simulators/neuron7.5/nrn' --with-nrnpython --with-paranrn --with-iv --disable-rx3d
+make && make install clean
+# Finally Install
 cd src/nrnpython && python setup.py install --home=/simulators/neuron7.5
+#
 # NOTE: For v7.5 ignore h3class.py error
 # https://neurojustas.wordpress.com/2018/03/27/tutorial-installing-neuron-simulator-with-python-on-ubuntu-linux/
 # https://www.neuron.yale.edu/phpBB/viewtopic.php?f=6&t=3834&p=16482&hilit=MetaHocObject#p16482
 #
+## Post-Installation Path configuration
 #echo "" >> ~/.bashrc
 #echo "# Add IV and NEURON to path variable" >> ~/.bashrc
 #echo -E "export PATH=\"/simulators/neuron7.5/iv/x86_64/bin:\$PATH\"" >> ~/.bashrc
@@ -43,31 +47,51 @@ cd src/nrnpython && python setup.py install --home=/simulators/neuron7.5
 #source ~/.bashrc
 ########################################################################
 
-#cd /
-#cd simulators
+cd / && cd simulators
 
 ################################ NEST ##################################
-# Install NEST setup
-#mkdir nest2.16 && cd nest2.16
-#wget "https://github.com/nest/nest-simulator/archive/v2.16.0.tar.gz"
-#tar xzf v2.16.0.tar.gz && cd nest-simulator-2.16.0
+# Make installation directory and go there
+mkdir nest2.16 && cd nest2.16
+# Download installation package in the created installation root directory
+wget "https://github.com/nest/nest-simulator/archive/v2.16.0.tar.gz"
+# Extract them and go to the extracted folder
+tar xzf v2.16.0.tar.gz && cd nest-simulator-2.16.0
 # Preinstall configure
-#cmake -DCMAKE_INSTALL_PREFIX:PATH=/simulators/nest2.16/
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/simulators/nest2.16/
 # Install
-#make && make install clean
+make && make install clean
 ########################################################################
 
+
+
 ################################ MOOSE #################################
+# Install pre-built package
 pip install pymoose
-# Install NEST setup
-#mkdir nest2.16 && cd nest2.16
-#wget "https://github.com/nest/nest-simulator/archive/v2.16.0.tar.gz"
-#tar xzf v2.16.0.tar.gz && cd nest-simulator-2.16.0
-# Preinstall configure
-#cmake -DCMAKE_INSTALL_PREFIX:PATH=/simulators/nest2.16/
-# Install
-#make && make install clean
+## To Build and install from it
+#cd / && cd simulators && mkdir moose3.1 && cd moose3.1
+#git clone https://github.com/BhallaLab/moose
+#cd moose/moose-core
+#mkdir _build && cd _build
+#cmake -DCMAKE_INSTALL_PREFIX:PATH=/simulators/moose3.1
+#make && ctest --output-on-failure
+#make install clean
 ########################################################################
+
+
+
+################################ BRIAN2 #################################
+# Install pre-built package
+#pip install brian2
+# Alternatively
+#pip install pyparsing sympy sphinxcontrib-issuetracker sphinx
+#
+#cd / && cd simulators && mkdir brian2-2.0.1 && cd brian2-2.0.1
+#git clone https://github.com/brian-team/brian2
+#cd brain2
+#python setup.py install --home=/simulators/brian2-2.0.1
+########################################################################
+
+
 
 ############################# POST-INSTALL #############################
 # create installed_sims.pth
@@ -76,3 +100,4 @@ pip install pymoose
 #/simulators/neuron7.5/lib/python
 # Add NEST to path
 #/simulators/nest2.16/lib/python2.7/site-packages
+
