5c083d3fc8f0af320d814ca331179bcbd67e3b3c
lungsi <lungsi@users.noreply.github.com>
2019-03-12 08:56:50 +0100

    added lines (commented) for Python3

--- docker-pynnmoose/base_install.sh
+++ docker-pynnmoose/base_install.sh
@@ -6,25 +6,32 @@
 # apt-get -qq update
 # apt-get install -y git
 # git clone https://github.com/systemsetup/myDockers.git
-# cd docker-pyNN_moose 
+# cd docker-pyNNmoose 
 # chmod +x base_install.sh
 # bash base_install.sh
+# NOTE: if you get an error redo "bash base_install.sh" [because of https://github.com/pypa/pip/issues/5240]
 
 # Base installations
-apt-get install -y wget bzip2 vim tmux lsof build-essential checkinstall libopenmpi-dev openmpi-bin openmpi-doc python2.7 python-pip python-tk python-lxml libhdf5-serial-dev
-# python-tk is for pylab and python-lxml libhdf5-serila-dev for NWB
+apt-get install -y wget bzip2 vim tmux lsof build-essential checkinstall libopenmpi-dev openmpi-bin openmpi-doc
+
+## python-tk is for pylab, python-lxml libhdf5-serila-dev for NWB, python-all-dev cython for NEST
+# for python2.7
+apt-get install -y python2.7 python-pip python-tk python-lxml libhdf5-serial-dev python-all-dev cython
+# for python3
+#apt-get install -y python3 python3-pip python3-tk python3-lxml libhdf5-serial-dev python3-dev cython3
 
 # Install for NEURON
 apt-get install -y libx11-dev libxext-dev mpich libncurses-dev
 
 # Install for NEST
-apt-get install -y cmake autoconf automake libtool libltdl7-dev libreadline6-dev libgsl0-dev gsl-bin python-all-dev cython
+apt-get install -y cmake autoconf automake libtool libltdl7-dev libreadline6-dev libgsl0-dev gsl-bin
 
 # Install for MOOSE
 apt-get install -y pkg-config libgraphviz-dev libhdf5-dev
 
-# Packges for setting virtual environments
+## Packges for setting virtual environments
+# for python2.7
 pip install --upgrade pip
 pip install -U pip setuptools virtualenv
-# because of https://github.com/pypa/pip/issues/5240
-# you may need to run bash base_install.sh AGAIN
+# for python3
+#pip3 install --upgrade pip setuptools virtualenv
